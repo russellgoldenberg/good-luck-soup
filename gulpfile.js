f1af81d5b901ea3191e356290609afc6f053565d
russell goldenberg <russellgoldenberg@gmail.com>
2014-06-14 19:46:10 -0400

    dev and prod gulp tasks built out

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,146 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload');
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	clean = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	cache = require('gulp-cache'),
+	livereload = require('gulp-livereload'),
+	fileinclude = require('gulp-file-include');
 
+var express = require('express');
+var LIVERELOAD_PORT = 35728;
+var EXPRESS_PORT = 5000;
 
-gulp.task('css', function() {
-  return gulp.src('src/css/main.scss')
-    .pipe(sass({ style: 'expanded' }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(notify({ message: 'CSS task complete' }));
+// dev tasks
+gulp.task('html-dev', function() {
+	return gulp.src('src/html/dev.html')
+		.pipe(fileinclude())
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('dev'));
 });
+
+gulp.task('css-dev', function() {
+	return gulp.src('src/assets/css/main.scss')
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('dev/assets/css'))
+});
+
+gulp.task('js-dev', function() {
+	return gulp.src('src/assets/js/**/*.js')
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('dev/assets/js'));
+});
+
+gulp.task('img-dev', function() {
+	return gulp.src('src/assets/img/**/*')
+		.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('dev/assets/img'));
+});
+
+
+gulp.task('clean-dev', function() {
+	return gulp.src(['dev/assets/css', 'dev/assets/js', 'dev/assets/img'], {read: false})
+		.pipe(clean());
+});
+
+gulp.task('default', ['clean-dev'], function() {
+	gulp.start('init-dev');
+});
+
+gulp.task('init-dev', ['html-dev','css-dev', 'js-dev', 'img-dev'], function() {
+	gulp.start('watch');
+});
+
+gulp.task('watch', function(callback) {
+	runServer(startLivereload);
+	callback && callback();
+});
+
+// prod tasks
+
+gulp.task('html-prod', function() {
+	return gulp.src('src/html/prod.html')
+		.pipe(fileinclude())
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('css-prod', function() {
+	return gulp.src('src/assets/css/main.scss')
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('dist/assets/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('js-prod', function() {
+	return gulp.src('src/assets/js/**/*.js')
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('dist/assets/js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('img-prod', function() {
+	return gulp.src('src/assets/img/**/*')
+		.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('dist/assets/img'));
+});
+
+gulp.task('clean-prod', function() {
+	return gulp.src(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], {read: false})
+		.pipe(clean());
+});
+
+gulp.task('prod', ['clean-prod'], function() {
+	gulp.start('css-prod', 'js-prod', 'img-prod');
+});
+
+
+// start server
+function runServer(callback) {
+
+	var app = express();
+	
+	app.use(express.static(__dirname + '/dev'));
+	app.listen(EXPRESS_PORT);
+
+	callback && callback();
+}
+
+// start livereload
+function startLivereload() {
+	var server = livereload(LIVERELOAD_PORT);
+
+	// Watch .html files
+	gulp.watch('src/html/**/*', ['html-dev']);
+
+	// Watch .scss files
+	gulp.watch('src/assets/css/**/*.scss', ['css-dev']);
+
+	// Watch .js files
+	gulp.watch('src/assets/js/**/*.js', ['js-dev']);
+
+	// Watch img files
+	gulp.watch('src/assets/img/**/*', ['img-dev']);
+
+	// Watch dev files and reload
+	gulp.watch(['dev/**/*']).on('change', function(file) {
+		server.changed(file.path);
+	});
+
+	console.log('Serving up a fresh batch of awesome!');
+}
---
+++ src/assets/js/good-luck-soup.js
@@ -0,0 +1,9 @@
+(function() {
+	log('v0.0.0');
+})();
+
+function log() {
+	if (window.console && console.log) {
+		console.log(msg);
+	}
+}
