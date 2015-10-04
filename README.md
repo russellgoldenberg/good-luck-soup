84a2831d83d196e539bc3f4c087a25f60fe512f8
russell goldenberg <russellgoldenberg@gmail.com>
2015-10-03 21:11:28 -0400

    stylus deployed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #Good Luck Soup Interactive
 An interactive documentary by Matthew Hashiguchi and Russell Goldenberg
+
+## NOTE:
+Must change filepath before prod task in typography
--- gulp-tasks/prod-css.js
+++ gulp-tasks/prod-css.js
@@ -1,29 +1,16 @@
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-// var autoprefixer = require('gulp-autoprefixer');
-// var minifycss = require('gulp-minify-css');
-// var rename = require('gulp-rename');
-// var notify = require('gulp-notify');
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
+var browserSync = require('browser-sync');
 
-// //compile all sass and autoprefix and minify
-// gulp.task('prod-css', function() {
-// 	gulp.src('src/css/main-dev.scss')
-// 		.pipe(sass().on('error', handleErrors))
-// 	    .pipe(autoprefixer({browsers:['last 2 versions'], cascade: false}))
-// 	    .pipe(minifycss())
-// 		.pipe(rename('main.css'))
-// 		.pipe(gulp.dest('.tmp/assets/css'));
-// });
-
-// var handleErrors = function() {
-// 	var args = Array.prototype.slice.call(arguments);
-// 	var error = args[0].message;
-
-// 	notify.onError({
-// 	title: 'Sass compile error',
-// 	message: 'Be better...'
-// 	}).apply(this, args);
-
-// 	console.log(error);
-// 	this.emit('end');
-// };
+//compile styl to css and autoprefix
+gulp.task('prod-css', function () {
+	gulp.src('src/css/main-dev.styl')
+        .pipe(stylus())
+		.pipe(autoprefixer())
+	    .pipe(minifycss())
+		.pipe(rename('main.css'))
+		.pipe(gulp.dest('.tmp/assets/css'));
+});
